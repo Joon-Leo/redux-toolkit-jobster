ee1578b80b5f161aa039a1291d5e31cd6e8d7d14
Joonsenia <128702580+Joon-Leo@users.noreply.github.com>
2026-01-23 22:49:23 +0900

    feat:Structure and Styled Components

---
+++ src/pages/Landing.js
@@ -0,0 +1,29 @@
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+
+const Landing = () => {
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="jobster Logo" className="logo" />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Praxis coloring book copper mug tote bag salvia helvetica grailed.
+            Master cleanse praxis same, cloud bread squid selvage vibecession
+            lyft kickstarter synth street art.
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </main>
+  );
+};
+
+export default Landing;
