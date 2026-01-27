60edc1dc9ddc627f66af1a2c4ad9726be48ad471
Joonsenia <128702580+Joon-Leo@users.noreply.github.com>
2026-01-28 01:53:38 +0900

    feat:Error page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,6 +1,7 @@
 import main from "../assets/images/main1.svg";
 import Wrapper from "../assets/wrappers/LandingPage";
 import { Logo } from "../components";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
@@ -19,7 +20,9 @@ const Landing = () => {
             Master cleanse praxis same, cloud bread squid selvage vibecession
             lyft kickstarter synth street art.
           </p>
-          <button className="btn btn-hero">Login/Register</button>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
