c1792c841b291bd6f7f2d5a003476c2a3c25a17e
Joonsenia <128702580+Joon-Leo@users.noreply.github.com>
2026-02-04 23:04:37 +0900

    feat(redux): add user slice, register and login async thunk

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react";
 import { Logo } from "../components";
 import Wrapper from "../assets/wrappers/RegisterPage";
 import FormRow from "../components/FormRow";
+import { toast } from "react-toastify";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser, registerUser } from "../features/user/userSlice";
 
 const initalStates = {
   name: "",
@@ -12,32 +15,47 @@ const initalStates = {
 
 const Register = () => {
   const [values, setValues] = useState(initalStates);
+  const { user, isLoading } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
     console.log(e.target);
   };
 
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e.target);
-    console.log("form submitted");
+    const { name, email, password, isMember } = values;
+    if (!password || !email || (!name && !isMember)) {
+      toast.error("Please fill out all fields");
+      return;
+    }
+    if (isMember) {
+      dispatch(loginUser({ email, password }));
+      return;
+    }
+    dispatch(registerUser({ name, email, password }));
   };
 
   return (
     <Wrapper className="full-page">
       <form className="form" onSubmit={handleSubmit}>
         <Logo />
-        <h3>Login</h3>
-        {/* name field */}
-        <FormRow
-          handleChange={handleChange}
-          type="text"
-          labelText="name"
-          name="name"
-          value={values.name}
-        />
+        <h3>{values.isMember ? "Login" : "Register"}</h3>
 
+        {!values.isMember && (
+          <FormRow
+            handleChange={handleChange}
+            type="text"
+            labelText="name"
+            name="name"
+            value={values.name}
+          />
+        )}
         {/* email field */}
         <FormRow
           handleChange={handleChange}
@@ -55,9 +73,16 @@ const Register = () => {
           name="password"
           value={values.password}
         />
+
         <button type="submit" className="btn btn-block">
           Submit
         </button>
+        <p>
+          {values.isMember ? "Not a member yet?" : "Already a member?"}
+          <button type="button" onClick={toggleMember} className="member-btn">
+            {values.isMember ? "Register" : "Login"}
+          </button>
+        </p>
       </form>
     </Wrapper>
   );
