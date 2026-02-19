d57fecf0a850e4e04bc7286bbbf8dddcb5da59dd
Joonsenia <128702580+Joon-Leo@users.noreply.github.com>
2026-02-19 17:37:56 +0900

    feat:Login APIthunk

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,17 +1,25 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
+import customFetch from "../../utils/axios";
 
 export const registerUser = createAsyncThunk(
   "user/registerUser",
   async (user, thunkAPI) => {
-    console.log(`Register User : ${JSON.stringify(user)}`);
+    try {
+      const response = await customFetch.post("/auth/register", user);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
   },
 );
 
 export const loginUser = createAsyncThunk(
   "user/loginUser",
   async (user, thunkAPI) => {
-    console.log(`Login User : ${JSON.stringify(user)}`);
+    try {
+      console.log(JSON.stringify(user));
+    } catch (error) {}
   },
 );
 
@@ -23,6 +31,22 @@ const initialState = {
 const userSlice = createSlice({
   name: "user",
   initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(registerUser.fulfilled, (state, { payload }) => {
+        const { user } = payload;
+        state.isLoading = false;
+        state.user = user;
+        toast.success(`Hello there ${user.name}`);
+      })
+      .addCase(registerUser.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        toast.error(payload);
+      });
+  },
 });
 
 export default userSlice.reducer;
