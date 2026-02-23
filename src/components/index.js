ee3097b4e78e6f1d05948de4968a6191a62de3a1
Joonsenia <128702580+Joon-Leo@users.noreply.github.com>
2026-02-23 22:13:15 +0900

    style:nav,side bar

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
+import BigSidebar from "./BigSidebar";
 import Logo from "./Logo";
+import Navbar from "./Navbar";
+import SmallSidebar from "./SmallSidebar";
+import FormRow from "./FormRow";
 
-export { Logo };
+export { Logo, FormRow, Navbar, BigSidebar, SmallSidebar };
